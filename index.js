facd9aa24e02fb4b8b9e26448637edb3abd0fcf2
HajimeNoJosh <joshuah.m.martin@gmail.com>
2020-05-08 15:51:42 -0400

    refactoring and structuring js projects

--- index.js
+++ index.js
@@ -1,36 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const usersRepo = require("./repositories/users");
+const cookieSession = require("cookie-session");
+const authRouter = require("./routes/admin/auth");
+const productsRouter = require("./routes/admin/products");
 
 const app = express();
 
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(
+  cookieSession({
+    keys: ["khjl234iou90vcxk8dfs3"],
+  })
+);
 
-app.get("/", (req, res) => {
-  res.send(`
-  <div>
-    <form method='POST'>
-        <input name='email' placeholder="email" />
-        <input name='password' placeholder="password" />
-        <input name='passwordconfirmation' placeholder="password confirmation" />
-        <button>Sign Up</button>
-    </form>
-  </div>
-  `);
-});
-
-app.post("/", async (req, res) => {
-  const { email, password, passwordConfirmation } = req.body;
-
-  const existingUser = await usersRepo.getOneBy({ email });
-  if (existingUser) {
-    return res.send("Email in use");
-  }
-  if (password !== passwordConfirmation) {
-    return res.send("Passwords must match");
-  }
-  res.send("account created!!!");
-});
+app.use(authRouter);
+app.use(productsRouter);
 
 app.listen(3000, () => {
   console.log("listening");
